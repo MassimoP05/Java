ec614463d2fbe46d0cb111e7a639609b8ade3fcf
MassimoP05 <s4094230@student.rmit.edu.au>
2025-02-20 14:14:09 +1100

    Add files via upload

---
+++ lab2.java
@@ -0,0 +1,22 @@
+public class lab2 {
+
+    public static void main(String[] args) {
+        int num1 = 233; 
+        int num2 = 11;
+        
+        double sum, diff, prod, quot,quot1,quot2,quot3;
+
+        sum = num1 + num2;
+        diff = num1 - num2;
+        prod = num1 * num2;
+        quot = num1 / num2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+
+
+    }
+}
