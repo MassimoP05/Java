9e841adbf70cec36853f45852ed62dede82f3ebd
MassimoP05 <s4094230@student.rmit.edu.au>
2025-02-20 14:05:33 +1100

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,14 @@
+class Example
+
+{
+    public static void main(String[] args)
+
+    {
+        
+        System.out.print("Hello, World! "); //This is printing the line Hello World 
+        System.out.println("how are you");
+        
+
+    }
+
+}
