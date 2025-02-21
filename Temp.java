a83e1d73fa2825bbb8b395a8506eebe2adf1dcba
Massimo Piva <s4094230@student.rmit.edu.au>
2025-02-21 21:55:58 +1100

    e

---
+++ Temp.java
@@ -0,0 +1,10 @@
+public class Temp {
+
+    public static void main(String[] args) {
+        double fahrenheit = 89, celsius;
+
+        celsius = ((5.0/9.0) * (fahrenheit - 32));
+
+        System.out.println(celsius);
+    }
+}
