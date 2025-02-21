a194f530d59278793c0b1c229319641b5f7c71d0
Massimo Piva <s4094230@student.rmit.edu.au>
2025-02-21 21:49:57 +1100

    e

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+
+    public static void main(String[] args) {
+        String firstName = "Massimo", lastName = "Piva", fullName;
+
+        fullName = firstName + " " + lastName;
+
+        System.out.println("hello, my name is " + fullName);
+    }
+}
