9412e1e3b462df759ccb34303893960fafdce620
Massimo Piva <s4094230@student.rmit.edu.au>
2025-02-21 21:45:16 +1100

    qe

--- Date.java
+++ Date.java
@@ -1,8 +1,12 @@
 public class Date {
 
     public static void main(String[] args) {
-        
+        String day = "Friday ", month = "February ";
+        int date = 21, year = 2025;
 
+        System.out.println("American Format: " + day + month + date + " " + year);
+        System.out.println("European Format: " + day + date + " " + month + year);
+        System.out.println("Japanese Format: " + year + " " + month + date + " " + day);;
     }
 
 }
