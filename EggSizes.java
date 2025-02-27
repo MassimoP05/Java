0f1b981fc0be6af7ce11abbe024001ca52e8e98f
Massimo Piva <s4094230@student.rmit.edu.au>
2025-02-27 13:47:52 +1100

    ut

---
+++ EggSizes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EggSizes {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        Double weight;
+        String size;
+
+        System.out.println("Weight of the Egg: ");
+        weight = myInput.nextDouble();
+
+
+        if (weight >= 72) {
+            size = "King-Size";
+        } else if (weight >= 68) {
+            size = "Jumbo";
+        } else if (weight >= 60) {
+            size = "Extra-Large";
+        } else if (weight >= 52) {
+            size = "Large";
+        } else if (weight >= 43) {
+            size = "Medium";
+        } else {
+            size = "Too Small";
+        }
+        System.out.println("The size of the egg is: " + size);
+        
+        myInput.close();
+    }
+}
