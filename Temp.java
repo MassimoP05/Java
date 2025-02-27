93c3291f09d9ae12a336e5ed19fde05ac4d6fe9f
Massimo Piva <s4094230@student.rmit.edu.au>
2025-02-27 12:44:52 +1100

    rty

--- Temp.java
+++ Temp.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Temp {
-
     public static void main(String[] args) {
-        double fahrenheit = 89, celsius;
+        Scanner myInput = new Scanner(System.in);
+        double fahrenheit, celsius;
 
+        System.out.println("What is the temp (in fahrenheit)?");
+        fahrenheit = myInput.nextDouble();
+        
         celsius = ((5.0/9.0) * (fahrenheit - 32));
 
         System.out.println(celsius);
+        myInput.close();
     }
 }
