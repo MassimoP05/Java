a8d72c4b7fa45ab16b3a4f2cac2eb3ff142ad97b
Massimo Piva <s4094230@student.rmit.edu.au>
2025-03-06 12:53:24 +1100

    ii

--- LoopCalc.java
+++ LoopCalc.java
@@ -4,13 +4,13 @@ public class LoopCalc {
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
         
-        int i, e;
+        int i, count;
         Double total = 0.0, num;
         
         System.out.println("How many numbers do you wanna count? ");
-        e = myInput.nextInt();
+        count = myInput.nextInt();
 
-        for (i = 1; i <= e; i++) { // Checking what number it is upto in the count
+        for (i = 1; i <= count; i++) { // Checking what number it is upto in the count
             System.out.print("Enter number " + i + ": "); // Prints out the count number
             num = myInput.nextDouble(); // makes num store the information of what number is inputed
             total += num; // (total = total + num) which adds the number to the total
