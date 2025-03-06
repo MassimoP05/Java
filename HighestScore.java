3c2fb78575b99a8e844ddd3f566a45e675f08b1c
Massimo Piva <s4094230@student.rmit.edu.au>
2025-03-06 12:16:36 +1100

    w

---
+++ HighestScore.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HighestScore {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        int classSize, i, studentNum = 0;
+        Double grade, highScore = 0.0;
+
+        System.out.println("How many students are in the class?");
+        classSize = myInput.nextInt();
+
+        for (i = 1; i <= classSize; i++){
+            System.out.println("Enter the " + i + " person's grade");
+            grade = myInput.nextDouble();
+
+            if (grade > highScore) {
+                highScore = grade;
+                studentNum = i;
+
+            }
+        }
+        System.out.println("Student " + studentNum + " has the highest grade: " + highScore);
+        
+    }
+}
