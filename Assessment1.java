49c739082f56dec12919a97f270eb2412e46e209
Massimo Piva <s4094230@student.rmit.edu.au>
2025-03-20 10:56:41 +1100

    qwe

---
+++ Assessment1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Assessment1 {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+
+    }
+}
---
+++ Assessment2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Assessment2 {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+
+    }
+}
