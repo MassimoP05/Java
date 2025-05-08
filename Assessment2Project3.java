65cd613ea76004cc9d57335eea4b4166f02d22a9
Massimo Piva <s4094230@student.rmit.edu.au>
2025-05-08 14:09:23 +1000

    w

--- Assessment2Project3.java
+++ Assessment2Project3.java
@@ -6,21 +6,22 @@ public class Assessment2Project3 {
         double tScore = 0;
         double oScore = 0;
 
+        
         // Game 1
         System.out.println("Game 1");
         System.out.print("Enter your team's score: ");
-        tScore += scanner.nextDouble();
+        tScore = scanner.nextDouble();
         System.out.print("Enter the opposition's score: ");
-        oScore += scanner.nextDouble();
+        oScore = scanner.nextDouble();
 
         double g1Percent = percentage(tScore, oScore);
         System.out.printf("Your team's percentage on scoring after Game 2 was: %.1f%%\n", g1Percent);
         // Game 2
         System.out.println("Game 2");
         System.out.print("Enter your team's score: ");
-        tScore += scanner.nextDouble();
+        tScore = scanner.nextDouble();
         System.out.print("Enter the opposition's score: ");
-        oScore += scanner.nextDouble();
+        oScore = scanner.nextDouble();
 
         double g2Percent = percentage(tScore, oScore);
         System.out.printf("Your team's percentage on scoring after Game 2 was: %.1f%%\n", g2Percent);
